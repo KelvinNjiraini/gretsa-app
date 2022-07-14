fffd314b51ccd74874232a33c7e1e49841c3cb8c
Kelvin Njiraini <njirainikelvin@gmail.com>
2022-07-14 16:44:39 +0300

    title change

--- src/components/Layout/TheHeader.vue
+++ src/components/Layout/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <nav>
-            <h1><router-link to="/">Home</router-link></h1>
+            <h1><router-link to="/">Job Applications Board</router-link></h1>
             <ul>
                 <li>
                     <router-link to="/feed">Feed</router-link>
